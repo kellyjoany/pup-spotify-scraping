28e6462a5d13f3614d109f1d408a0e67ee985e20
kelly <kelly.joany@gmail.com>
2020-02-13 15:56:30 -0300

    alteracao no teste (convertia [] em {} )

--- __tests__/integration/log.test.js
+++ __tests__/integration/log.test.js
@@ -4,14 +4,6 @@ import app from '../../src/app';
 require('dotenv/config');
 
 describe('Log', () => {
-  it('should be able to store results in log file', async () => {
-    const response = await request(app)
-      .post('/log')
-      .send();
-
-    expect(response.status).toBe(201);
-  });
-
   it('should be able to get the results in log file', async () => {
     const response = await request(app)
       .get('/log')
